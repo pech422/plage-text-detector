2f1bd0f744b68d487e8b77f85dca5d6d3294a076
RoachLok <jtabordajunior@hotmail.com>
2022-05-16 19:42:18 +0200

    Add api endpoint to query user info. Returns json. By pech422 <pech422@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import re
+import json
 
 app = Flask(__name__)
+
+
+def readAgenda():
+    agenda = []
+    filepath = "data/agenda.csv"
+    with open(filepath, 'r') as fp:
+        line = fp.readline()
+        while line:
+            line = fp.readline()
+            agenda.append(line.split(',', 2))
+
+    return agenda
+
 
 @app.route("/")
 def index():
@@ -11,10 +25,6 @@ def index():
 def text_simi():
     f = open("data/agenda.csv", 'r')
     agenda = []
-    '''agenda2 = []
-    for i in range(len(agenda)):
-        if (agenda[i] != ","):
-            agenda2[j] += agenda[i]'''
 
     filepath = "data/agenda.csv"
     with open(filepath, 'r') as fp:
@@ -23,9 +33,27 @@ def text_simi():
             line = fp.readline()
             agenda.append(line.split(',', 2))
     
-    #agenda = re.sub(r"(\\n']|\['|')', '", agenda[3])
     return render_template("text_simi.html", test=agenda)
+
+@app.route("/api/get_user/<user>")
+def getUser(user):
+
+    for contacto in agenda:
+        if (user == contacto[0]):
+
+            x = {
+                    "name"   : contacto[0],
+                    "number" : contacto[1]
+                }
+
+            return x
+
+
+    return jsonify(usuario=user, error="User not found."), 404
 
 
 if __name__ == '__main__':
+    # Pass by value pass by reference
+    agenda = readAgenda()
+    
     app.run(debug=True, host='0.0.0.0')
