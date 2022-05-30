889962f814f0fdfc7f7517e3cdb904c6c1da4764
RoachLok <jtabordajunior@hotmail.com>
2022-05-30 19:53:02 +0200

    Read agenda just once. By: pech422 <pech422@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+from ast import List
 from flask import Flask, render_template, jsonify
 import re
 import json
+import pandas as pd
 
 app = Flask(__name__)
 
 
-def readAgenda():
+contact = ["name", "123414", "1999-10-1", "1"]
+
+def readAgenda(names: List):
     agenda = []
     filepath = "data/agenda.csv"
+
     with open(filepath, 'r') as fp:
         line = fp.readline()
         while line:
             line = fp.readline()
-            agenda.append(line.split(',', 2))
+            if line:
+                agenda.append(line.split(',', 4))
 
     return agenda
 
@@ -37,21 +43,37 @@ def text_simi():
 
 @app.route("/api/get_user/<user>")
 def getUser(user):
-
     for contacto in agenda:
         if (user == contacto[0]):
-
             x = {
                     "name"   : contacto[0],
-                    "number" : contacto[1]
+                    "number" : contacto[1],
+                    "birth"  : contacto[2],
+                    "gender" : contacto[3][:1]
                 }
-
             return x
-
 
     return jsonify(usuario=user, error="User not found."), 404
 
+@app.route("/api/get_adults/<edad>")
+def getAdults(edad):
+    adults = []
+    for contacto in agenda:
+        year = int(contacto[2][:4])
+        edad = int(edad)
+        if (2022-year < edad):
+            var = {
+                    "name"   : contacto[0],
+                    "number" : contacto[1],
+                    "birth"  : contacto[2],
+                    "gender" : contacto[3]
+                }
+                
+            adults.append(var)
 
+    return jsonify(adults) if adults else jsonify(edad=edad, error="Age not found."), 404
+
+    
 if __name__ == '__main__':
     # Pass by value pass by reference
     agenda = readAgenda()
