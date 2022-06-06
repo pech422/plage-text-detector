c0f13533f789f42af1f9d17612910ed72f6b73bc
RoachLok <jtabordajunior@hotmail.com>
2022-06-06 19:47:04 +0200

    Add Contact class and return as json.
    By: pech422 <pech422@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from ast import List
 from flask import Flask, render_template, jsonify
 import re
 import json
 import pandas as pd
+from Contact import Contact
 
 app = Flask(__name__)
 
 
-contact = ["name", "123414", "1999-10-1", "1"]
+contact1 = Contact("Cheri Batson","06243158218-21","1977-1-19","1")
 
-def readAgenda(names: List):
+def readAgenda():
     agenda = []
     filepath = "data/agenda.csv"
 
@@ -18,7 +18,8 @@ def readAgenda(names: List):
         while line:
             line = fp.readline()
             if line:
-                agenda.append(line.split(',', 4))
+                aux = line.split(',', 4)
+                agenda.append(Contact(aux[0],aux[1],aux[2],aux[3]))
 
     return agenda
 
@@ -27,49 +28,32 @@ def readAgenda(names: List):
 def index():
     return render_template("index.html")
 
+
+contactos = []
+
 @app.route("/text_simi")
 def text_simi():
-    f = open("data/agenda.csv", 'r')
-    agenda = []
+    for i in range(len(agenda)):
+        contactos.append(agenda[i].to_json())
 
-    filepath = "data/agenda.csv"
-    with open(filepath, 'r') as fp:
-        line = fp.readline()
-        while line:
-            line = fp.readline()
-            agenda.append(line.split(',', 2))
-    
-    return render_template("text_simi.html", test=agenda)
+    return jsonify(contactos)
 
 @app.route("/api/get_user/<user>")
 def getUser(user):
-    for contacto in agenda:
-        if (user == contacto[0]):
-            x = {
-                    "name"   : contacto[0],
-                    "number" : contacto[1],
-                    "birth"  : contacto[2],
-                    "gender" : contacto[3][:1]
-                }
-            return x
+    for i in range(len(agenda)):
+        name = agenda[i].getName()
+        if (user == name):
+            return agenda[i].to_json()
 
     return jsonify(usuario=user, error="User not found."), 404
 
 @app.route("/api/get_adults/<edad>")
 def getAdults(edad):
     adults = []
-    for contacto in agenda:
-        year = int(contacto[2][:4])
-        edad = int(edad)
-        if (2022-year < edad):
-            var = {
-                    "name"   : contacto[0],
-                    "number" : contacto[1],
-                    "birth"  : contacto[2],
-                    "gender" : contacto[3]
-                }
-                
-            adults.append(var)
+    for contact in agenda:
+        if (contact.isAdult(edad)):
+            adults.append(contact.to_json())
+
 
     return jsonify(adults) if adults else jsonify(edad=edad, error="Age not found."), 404
 
